283c484f660a68173571479d47673f84c439d72d
camitr <shriamit1@gmail.com>
2014-11-04 15:49:07 +0530

    added comma seprator

--- printlogger.sh
+++ printlogger.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sed -i 's/\///g' 2
+awk '{print $1 "," $4 "," $2 "," $9 "," $10$11$12$13$14$15$16$17$18}' page_log.1|tr -d [] > pagelog
+
+
+
+sed -i 's/\///g' pagelog
 
 
 while IFS=, read -ra arr; do
@@ -12,4 +16,4 @@ dat=$(echo $dat ${arr[1]:10})
 	    echo "INSERT INTO print_detail (printer,date,user,IP,info) VALUES ('${arr[0]}','$dat','${arr[2]}','${arr[3]}', '${arr[4]}');"
 
 done < 2 | mysql -uroot -p123 printer 
-sed  -i 's/ /:/' 2
+sed  -i 's/ /:/' pagelog
