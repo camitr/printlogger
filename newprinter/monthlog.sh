e2c498ba0fe19d0429673eae1e298bc741041054
camitr <shriamit1@gmail.com>
2015-06-03 14:11:59 +0530

    update user list gui

--- newprinter/monthlog.sh
+++ newprinter/monthlog.sh
@@ -4,7 +4,6 @@
 monthly_print(){
 
 echo $file
-#echo "Total number of pages printed in this month =" `wc -l $file|awk {'print $1'}`
 total_pages=`wc -l $file|awk {'print $1'}`
 zenity --info --title="Total page print" --text="$total_pages"
 
@@ -17,9 +16,7 @@ total_user(){
 	
 	user_total=${#user_list[@]}
 
-	echo "Total user in this month =:" $user_total 
 	
-	echo "List of user::"
 #	printf "%s\n" "${user_list[@]}"
 
 	zenity --list  --title="List of user"  \
@@ -29,16 +26,22 @@ total_user(){
 
 
 page_count(){
+	arr=()
+	idx=0
 	for i in ${user_list[@]}
 	do
-		printf "Number of print by $i are =: "
-		cat $file| grep $i |wc -l
+       	#	printf "Number of print by $i are =:" 
+	 # cat $file| grep $i |wc -l
+	 arr[idx]=($(grep $i < $file| wc -l))
+	 idx=$((idx+1))
 	done
+
+	print ${arr[1]}
+#	print ${user_print[@]}
 }
 
 
 
-#printf " Welcome to the Monthly log details\n"
 zenity --info --title="Monthly page logger" --text="Welcome to printer log detail, Please give file name in next window"
 file=$(zenity --entry --title="Monthly logger" --text="File name:")
 total_user
